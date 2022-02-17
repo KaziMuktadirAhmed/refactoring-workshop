79211cb13eabd9df30ee330a2811d63e92c0bb7a
Kazi Muktadir Ahmed <bsse1111@iit.du.ac.bd>
2022-02-17 06:18:56 +0600

    Update FizzBuzz.java

--- java/src/main/java/workshop/FizzBuzz/FizzBuzz.java
+++ java/src/main/java/workshop/FizzBuzz/FizzBuzz.java
@@ -4,19 +4,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
- * Requirements:
- * For factor of three print Fizz instead of the number
- * For factor of five print Buzz instead of the number
- * For numbers which are factors of both three and five print FizzBuzz instead of the number
- */
 public class FizzBuzz {
     private List<Matcher> matchers;
     private Response nullResponse;
 
     public FizzBuzz () {
         super();
-        this.matchers = new ArrayList<Matcher>(Arrays.asList(new FizzPattern(), new BuzzPattern(), new NotMatchedPattern()));
+        this.matchers = new ArrayList<>(Arrays.asList(new FizzPattern(), new BuzzPattern(), new NotMatchedPattern()));
         this.nullResponse = new NullResponse();
     }
 
